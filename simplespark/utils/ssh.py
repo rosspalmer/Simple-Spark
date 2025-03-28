50cf6ff8a3c2ae9eb6d5fa7129ca1f1aee4a04f2
Ross Palmer <rossbpalmer@gmail.com>
2025-03-28 13:29:24 -0600

    add error throwing

--- simplespark/utils/ssh.py
+++ simplespark/utils/ssh.py
@@ -61,6 +61,9 @@ class SSHUtils:
 
         return last_part in root_folder_contents
 
-    def run(self, command: str):
+    def run(self, command: str, throw_exception: bool = True):
         stdin, stdout, stderr = self.ssh.exec_command(command)
+        error_lines = stderr.readlines()
+        if throw_exception and len(error_lines) > 0:
+            raise Exception(f"SSH run error: {error_lines[0].strip()}")
         return stdin, stdout, stderr
